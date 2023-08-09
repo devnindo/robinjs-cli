97d96ed1e32b9b09207ff69f5a2054ea71597e38
Shakil Ahmed <sasajib@gmail.com>
2023-08-09 16:01:50 +0600

    modify create vite initialization

--- src/cli-command.ts
+++ src/cli-command.ts
@@ -1,11 +1,30 @@
 import {execSync} from 'child_process'
 import command from './command-mapper'
+import path from 'node:path'
+import * as fs from 'fs'
 
 
 class CLI {
     @command('create')
     createProject(projectName: string): void {
-        execSync(`npx create-vite ${projectName} --template react-ts`, {stdio: 'inherit'})
+
+        const createVitePath = path.resolve(__dirname, '../node_modules/.bin/create-vite')
+        execSync(`${createVitePath} ${projectName} --template react-ts`, {stdio: 'inherit'})
+
+
+        const projectPath = path.resolve(process.cwd(), projectName)
+
+
+        if (fs.existsSync(path.join(projectPath, 'yarn.lock'))) {
+            console.log('Detected Yarn. Installing dependencies using Yarn...')
+            execSync('yarn install', {stdio: 'inherit', cwd: projectPath})
+        } else if (fs.existsSync(path.join(projectPath, 'package-lock.json'))) {
+            console.log('Detected npm. Installing dependencies using npm...')
+            execSync('npm install', {stdio: 'inherit', cwd: projectPath})
+        } else {
+            console.log('Using npm as the default. Installing dependencies using npm...')
+            execSync('npm install', {stdio: 'inherit', cwd: projectPath})
+        }
     }
 
     @command('dev')
