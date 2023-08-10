40ff500cf3ea974444253fbfa3e35dcef0dff7b3
Shakil Ahmed <sasajib@gmail.com>
2023-08-10 19:16:41 +0600

    implement basic configuration file system and error handling

--- src/cli-command.ts
+++ src/cli-command.ts
@@ -4,45 +4,66 @@ import fs from 'fs-extra'
 import command from './command-mapper'
 import {PackageModifier} from './package-modifier'
 
-
 class CLI {
     private readonly CONFIG_FILE_NAME = 'robin.config.ts'
 
     @command('create')
     createProject(projectName: string): void {
-        const templatePath = path.resolve(__dirname, '../template/react-ts')
-        const projectPath = path.resolve(process.cwd(), projectName)
+        try {
+            const templatePath = path.resolve(__dirname, '../template/react-ts')
+            const projectPath = path.resolve(process.cwd(), projectName)
 
-        fs.copySync(templatePath, projectPath)
-        const newPackageJsonPath = path.join(projectPath, 'package.json')
-        PackageModifier.modifyPackageJson(newPackageJsonPath, {name: projectName})
+            fs.copySync(templatePath, projectPath)
+            const newPackageJsonPath = path.join(projectPath, 'package.json')
+            PackageModifier.modifyPackageJson(newPackageJsonPath, {name: projectName})
 
-        console.log('Using npm as the default. Installing dependencies using npm...')
-        execSync('npm update -y', {stdio: 'inherit', cwd: projectPath})
+            console.log('Using npm as the default. Installing dependencies using npm...')
+            execSync('npm update -y', {stdio: 'inherit', cwd: projectPath})
+        } catch (error: any) {
+            this.handleCommandError('createProject', error)
+        }
     }
-
 
     @command('dev')
-    async startDevServer(): Promise<void> {
-        const config = require(path.resolve(process.cwd(), this.CONFIG_FILE_NAME))
-        const port = config.dev.port
-        const args = process.argv.slice(3).join(' ')
-        execSync(`npx vite --port ${port} ${args}`, {stdio: 'inherit'})
+    startDevServer(): void {
+        try {
+            const config = require(path.resolve(process.cwd(), this.CONFIG_FILE_NAME))
+            const port = config.dev.port
+            const args = process.argv.slice(3).join(' ')
+            execSync(`npx vite --port ${port} ${args}`, {stdio: 'inherit'})
+        } catch (error: any) {
+            this.handleCommandError('startDevServer', error)
+        }
     }
-
 
     @command('build')
     buildProject(): void {
-        const args = process.argv.slice(3).join(' ')
-        execSync(`npx vite build ${args}`, {stdio: 'inherit'})
+        try {
+            const args = process.argv.slice(3).join(' ')
+            execSync(`npx vite build ${args}`, {stdio: 'inherit'})
+        } catch (error: any) {
+            this.handleCommandError('buildProject', error)
+        }
     }
 
     @command('start')
     serveProject(): void {
-        const config = require(path.resolve(process.cwd(), this.CONFIG_FILE_NAME))
-        const port = config.production.port
-        const args = process.argv.slice(3).join(' ')
-        execSync(`npx vite preview --port ${port} ${args}`, {stdio: 'inherit'})
+        try {
+            const config = require(path.resolve(process.cwd(), this.CONFIG_FILE_NAME))
+            const port = config.production.port
+            const args = process.argv.slice(3).join(' ')
+            execSync(`npx vite preview --port ${port} ${args}`, {stdio: 'inherit'})
+        } catch (error: any) {
+            this.handleCommandError('serveProject', error)
+        }
+    }
+
+    private handleCommandError(methodName: string, error: any) {
+        if (error.signal === 'SIGINT' || error.message.includes('Command failed')) {
+            console.info(`CTRL + C pressed, exiting...`)
+        } else {
+            console.error(`Error executing ${methodName}:`, error.message)
+        }
     }
 }
 
